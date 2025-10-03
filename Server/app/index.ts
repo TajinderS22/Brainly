aa23077bc27ab84a40b218ddb846efaadc970986
Tajinder Singh <singhtajinder9888@gmail.com>
2025-10-03 23:09:26 +0530

    updated BackendURL all 05

--- Server/app/index.ts
+++ Server/app/index.ts
@@ -17,10 +17,12 @@ app.use(express.json())
 
 app.use('/api/v1/user',userRouter)
 
-
+app.get("/",(req,res)=>{
+    res.send("Hi Tajinder")
+})
 
 const Main=async ()=>{    
-    await mongoose.connect( MONGO_DB_URL+'BrinlyApp')
+    await mongoose.connect( process.env.MONGO_DB_URL+'BrinlyApp')
     
 
     app.listen(port,()=>{
