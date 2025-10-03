85dffbe90fc5e041779537f479f22b594145e484
Tajinder Singh <singhtajinder9888@gmail.com>
2025-10-03 23:40:39 +0530

    Backend 001

--- client/Brainly-app/src/utils/Backend.ts
+++ client/Brainly-app/src/utils/Backend.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL='http://api.brainly.tajinder.xyz'
-export const frontEndUrl='http://brainly.tajinder.xyz'
+export const BACKEND_URL='https://api.brainly.tajinder.xyz'
+export const frontEndUrl='https://brainly.tajinder.xyz'
