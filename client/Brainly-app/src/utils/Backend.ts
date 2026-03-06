c24904ed8d81cdcb4c83b0d1aebe3eb3ca0cf867
TajinderS22 <singhtajinder9888@gmail.com>
2026-03-06 22:38:36 +0530

    Updated Origin Server

--- client/Brainly-app/src/utils/Backend.ts
+++ client/Brainly-app/src/utils/Backend.ts
@@ -1,2 +1,6 @@
-export const BACKEND_URL='https://api.brainly.tajinder.in'
+// export const BACKEND_URL='https://api.brainly.tajinder.in'
 export const frontEndUrl='https://brainly.tajinder.in'
+
+
+// *************cloudflared Tunnels ***********
+export const BACKEND_URL='https://apibrainly.tajinder.in'
