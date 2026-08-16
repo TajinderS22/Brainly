19ab2a9762e24be5a552f94efeefccfeef31ab65
Tajinder Singh <singhtajinder9888@gmail.com>
2026-08-16 23:59:33 +0530

    branching for easy maintainance

--- Server/app/config.ts
+++ Server/app/config.ts
@@ -5,8 +5,7 @@ dotenv.config();
 const jwtSecret =  process.env.JWT_USER_PASSWORD;
 
 if (!jwtSecret) {
-  throw new Error('❌ JWT_USER_PASSWORD is not set in your .env file');
+  throw new Error(' JWT_USER_PASSWORD is not set in your .env file');
 }
 
-// ✅ Tell TypeScript this is a guaranteed string now
 export const JWT_SECRET: string = jwtSecret;
